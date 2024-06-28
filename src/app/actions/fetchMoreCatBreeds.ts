018f2bcbb1095771eb9932675d0f62e9ada4b6aa
niveditabhat <niveditabhat@gmail.com>
2024-06-28 08:54:48 +0200

    Fetch cat breed lists

--- src/app/actions/fetchMoreCatBreeds.ts
+++
@@ -1,6 +0,0 @@
-"use server";
-import fetchCatBreeds from "../lib/fetchCatBreeds";
-
-export const fetchMoreCatBreeds = async (limit: number, offset: number) => {
-  return fetchCatBreeds(limit, offset);
-};
